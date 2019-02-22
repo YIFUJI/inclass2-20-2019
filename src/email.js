b3c965da0df86d8af8e2c02f7472a40f33f80f44
JI YIFU <yji1@buffs.wtamu.edu>
2019-02-21 21:38:11 -0600

    first commit

---
+++ src/email.js
@@ -0,0 +1,12 @@
+class Email extends React.Component {
+    state = {}
+    render() {
+        return (
+            <div class="form-group">
+                <label for="exampleInputEmail1">Email address</label>
+                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
+            </div>
+
+        );
+    }
+}
